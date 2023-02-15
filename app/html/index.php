2fddc10efd699e5a072140a6c4c6eaa4662055cf
fernando flores <fernando_flores@ucol.mx>
2023-02-15 16:17:55 -0600

    primer commit

---
+++ app/html/index.php
@@ -0,0 +1,17 @@
+<?php
+$servername = "148.213.50.148:3306" ;
+$database = "prueba1" ;
+$username = "root" ;
+$password = "1234" ;
+
+// Crear conexión:
+$conn = mysqli_connect ($servername, $username, $password,$database);
+
+// verificar conexión:
+if (!$conn) {
+	die ("Falló la conexión: " . mysqli_connect_error());
+}
+
+echo "Conexión existosa" ;
+mysqli_close ($conn);
+?>
